13d04c3dcab1ee577c095a70e3aed673f01c7aea
vinay <vinay.gulani@relishly.com>
2015-11-15 21:12:48 -0800

    cc

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def index():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5050))
     app.run(host='127.0.0.1', port=port)
     
 
